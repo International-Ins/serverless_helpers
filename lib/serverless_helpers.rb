90e69e0ffc65ba369b67f8bc06e8715f84dfdae5
Jack M <jack.mangels@nfp.com>
2019-11-07 07:26:36 -0700

    Create parser so symbols can be converted to classes and verified.

--- lib/serverless_helpers.rb
+++ lib/serverless_helpers.rb
@@ -10,5 +10,42 @@ module ServerlessHelpers
   class Error < StandardError; end
 
   extend Dry::Configurable
-  setting :event_parser, Event::Parser::Plain.new
+  setting(:event_parser, :plain) do |value|
+    SymbolToClassParser.call(
+      argument: value,
+      type: :event,
+      namespace: :parser,
+      base_class: ServerlessHelpers::Event::Parser::Base
+    )
+  end
+
+  private
+  
+  class SymbolToClassParser
+    def self.call(argument:, type:, namespace:, base_class:)
+      if argument.is_a? Symbol
+        create_class(type, namespace, argument).new
+      elsif argument.is_a? base_class
+        argument
+      else
+        raise ArgumentError.new("Object, #{argument}, is not a sub-class of #{base_class.name}.")
+      end
+    rescue NameError => e
+      raise NameError.new("Could not find #{argument} in namespace #{namespace_for(type, namespace)}: #{e.message}")
+    end
+
+    private
+
+    def self.create_class(type, namespace, class_name)
+      "#{namespace_for(type, namespace)}::#{to_camel(class_name)}".constantize
+    end
+
+    def self.namespace_for(type, namespace)
+      "ServerlessHelpers::#{to_camel(type)}::#{to_camel(namespace)}"
+    end
+
+    def self.to_camel(sym)
+      "#{sym.to_s.camelize}"
+    end
+  end
 end
